e1f8eaf23e95abc14723a0e1ed857eeb7883b787
dnakahara1 <77356462+dnakahara1@users.noreply.github.com>
2021-12-13 14:44:24 -0800

    Delete Front End directory

--- Front End/css/visitor-counter.js
+++
@@ -1,5 +0,0 @@
-function visitorCounter(){
-	fetch("https://hggxuw0td9.execute-api.us-east-2.amazonaws.com/visitorCounter").then(response => response.text()).then((body) => {
-		document.getElementById("visitor-counter").innerHTML=body
-	})
-}
